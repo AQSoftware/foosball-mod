02aee2e978ad99d474cb85c7858e97ffb9abe07e
lance <lanceting@bengga.co>
2018-08-07 19:59:58 +0800

    uncommented uploading to s3

--- deploy.sh
+++ deploy.sh
@@ -14,5 +14,5 @@ fi
 rm -f package.zip
 git config user.name > version && git config user.email >> version && git log --pretty=format:'%h%n' -n 1 >> version
 zip -x *.sh -r package.zip *
-# aws s3 sync build/ s3://bengga-web-funtypes/production/$1
+aws s3 sync build/ s3://bengga-web-funtypes/production/$1
 shasum package.zip
